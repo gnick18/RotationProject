aaa24488d1828717092cabcad9aa7de914fcdf68
Grant Nickles <gnickles@Nymeria.local>
2020-10-15 20:36:11 -0500

    Changed $1 in the file to $Name

--- Rename.sh
+++ Rename.sh
@@ -32,29 +32,29 @@ IsoG=$Name"ISOmFu.fa"
 #cp /home/gnickles/Genomes/"$IsoG" .
 #
 #
-gffread "$Iso" -x CDS_$1"$Tag".fa -g "$IsoG"
+gffread "$Iso" -x CDS_$Name"$Tag".fa -g "$IsoG"
 gffread "$Reff" -x CDS_1"$Tag".fa -g "$ReffG"
 #
 #
 translate_nucl_to_prot_fasta.py CDS_1"$Tag".fa > 1"$Tag"_prot_renamed.fa
-translate_nucl_to_prot_fasta.py CDS_$1"$Tag".fa > $1"$Tag"_prot.fa
+translate_nucl_to_prot_fasta.py CDS_$Name"$Tag".fa > $Name"$Tag"_prot.fa
 #
 #
 makeblastdb -dbtype prot -in 1"$Tag"_prot_renamed.fa
-makeblastdb -dbtype prot -in $1"$Tag"_prot.fa
+makeblastdb -dbtype prot -in $Name"$Tag"_prot.fa
 #
 #
 echo "Starting first blast"
 date +%R
 #
-blastp -query 1"$Tag"_prot_renamed.fa -db $1"$Tag"_prot.fa -outfmt 6 -num_threads 3 -out REFF_vs_ISO_prots.bls
+blastp -query 1"$Tag"_prot_renamed.fa -db $Name"$Tag"_prot.fa -outfmt 6 -num_threads 3 -out REFF_vs_ISO_prots.bls
 #
 #
 echo "First blast done"
 date +%R
 #
 #
-blastp -query $1"$Tag"_prot.fa -db 1"$Tag"_prot_renamed.fa -outfmt 6 -num_threads 3 -out ISO_vs_REFF_prots.bls
+blastp -query $Name"$Tag"_prot.fa -db 1"$Tag"_prot_renamed.fa -outfmt 6 -num_threads 3 -out ISO_vs_REFF_prots.bls
 #
 #
 echo "Second blast done"
@@ -64,19 +64,19 @@ date +%R
 reciprocal_best_blast.py ISO_vs_REFF_prots.bls REFF_vs_ISO_prots.bls 1 2 12 high ISO_reference_rbh.txt
 #
 #
-rename_genes_blast.py $1"$Tag" CDS_$1"$Tag".fa "$Iso" ISO_reference_rbh.txt > $1ISO_names
+rename_genes_blast.py $Name"$Tag" CDS_$Name"$Tag".fa "$Iso" ISO_reference_rbh.txt > $NameISO_names
 #
 #
-rename_gff.py $1ISO_names "$Iso" > $1"$Tag"_genes_renamed.gff
+rename_gff.py $NameISO_names "$Iso" > $Name"$Tag"_genes_renamed.gff
 #
 #
-list_genes_by_scaff.py $1"$Tag"_genes_renamed.gff > $1"$Tag"_genes_in_order.txt
+list_genes_by_scaff.py $Name"$Tag"_genes_renamed.gff > $Name"$Tag"_genes_in_order.txt
 #
 #
-rename_fasta.py $1ISO_names CDS_$1"$Tag".fa > $1"$Tag"_cds_renamed.fa
-rename_fasta.py $1ISO_names $1"$Tag"_prot.fa > $1"$Tag"_prot_renamed.fa
+rename_fasta.py $NameISO_names CDS_$Name"$Tag".fa > $Name"$Tag"_cds_renamed.fa
+rename_fasta.py $NameISO_names $Name"$Tag"_prot.fa > $Name"$Tag"_prot_renamed.fa
 #
-#convert_augustus_gff_and_fasta_to_gbk.py "$IsoG" $1"$Tag"_genes_renamed.gff $1"$Tag"_prot_renamed.fa > $1"$Tag"_genome.gbk
+#convert_augustus_gff_and_fasta_to_gbk.py "$IsoG" $1"$Tag"_genes_renamed.gff $1"$Tag"_prot_renamed.fa > $Name"$Tag"_genome.gbk
 #
 cp *renamed.fa /home/gnickles/
 cp *renamed.gff /home/gnickles/
